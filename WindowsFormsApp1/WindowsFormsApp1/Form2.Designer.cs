cde79eacd8a41bd4648cb4f28679f115ebbe7086
YefimSk <63807701+YefimSk@users.noreply.github.com>
2021-11-22 16:32:18 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Snow;
+            this.textBox1.Location = new System.Drawing.Point(6, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Зашифровка файла и расшифровка";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.Location = new System.Drawing.Point(242, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 141);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Информация о файле";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(221, 23);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "awsPay165FcfyTf6";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ключ";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Peru;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Зашифровать";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Peru;
+            this.button4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(150, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 24);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Расшифровать";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Salmon;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aESToolStripMenuItem,
+            this.dESToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(348, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aESToolStripMenuItem
+            // 
+            this.aESToolStripMenuItem.Name = "aESToolStripMenuItem";
+            this.aESToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.aESToolStripMenuItem.Text = "AES";
+            this.aESToolStripMenuItem.Click += new System.EventHandler(this.aESToolStripMenuItem_Click);
+            // 
+            // dESToolStripMenuItem
+            // 
+            this.dESToolStripMenuItem.Name = "dESToolStripMenuItem";
+            this.dESToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.dESToolStripMenuItem.Text = "DES";
+            this.dESToolStripMenuItem.Click += new System.EventHandler(this.dESToolStripMenuItem_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(348, 217);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "AES/DES";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dESToolStripMenuItem;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp1
+{ 
+    public partial class Form2 : Form
+    {
+       public int Flag = 0;// флаг для переключения AES/DES функций
+                           // 0- AES 1-DES
+        private byte[] iv3 = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        private byte[] iv = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Flag == 0)//AES
+            {
+
+                // Open file to Encrypt
+                openFileDialog1.Title = "Select the source file to encrypt";
+                openFileDialog1.Filter = "All files (*.*)|*.*";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    button1.Enabled = true;
+                }
+
+            }
+
+            else if (Flag == 1)//DES 
+            {
+                // Open file to Encrypt
+                openFileDialog1.Title = "Select the source file to encrypt";
+                openFileDialog1.Filter = "All files (*.*)|*.*";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    button1.Enabled = true;
+                }
+
+            }
+
+        }
+
+        //private void button2_Click(object sender, EventArgs e)//мне это не нужно
+        //{
+        //    if (Flag == 0)//AES
+        //    {
+        //        // Open file to Encrypt
+        //        openFileDialog1.Title = "Select the source file to decrypt";
+        //        openFileDialog1.Filter = "Encrypted Files (*.enc)|*.enc";
+        //
+        //        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        //        {
+        //            textBox2.Text = openFileDialog1.FileName;
+        //            button2.Enabled = true;
+        //        }
+        //    }
+        //    else if (Flag == 1)//DES 
+        //    {
+        //        // Open file to Encrypt
+        //        openFileDialog1.Title = "Select the source file to decrypt";
+        //        openFileDialog1.Filter = "Encrypted Files (*.enc)|*.enc";
+        //
+        //        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        //        {
+        //            textBox2.Text = openFileDialog1.FileName;
+        //            button2.Enabled = true;
+        //        }
+        //    }
+        //}
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Flag == 0)// AES
+            {
+                if (EncryptData(textBox1.Text, textBox1.Text + ".aesenrypt", textBox3.Text) == true)
+                    MessageBox.Show(this, "Done!", "Encryption Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(this, "The encryption process failed!", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if (Flag == 1) //DES
+            {
+                if (EncryptData(textBox1.Text, textBox1.Text + ".desencrypt", textBox3.Text) == true)
+                    MessageBox.Show(this, "Done!", "Encryption Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(this, "The encryption process failed!", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)// здаесь заменить текстбокс со втрого на первый
+        {
+            if (Flag == 0)//AES
+            {
+                string textBox1Name = textBox1.Text.Replace(".aesenrypt", "");
+                if (DecryptData(textBox1.Text, textBox1Name, textBox3.Text) == true)
+                    MessageBox.Show(this, "Done!", "Decryption Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(this, "The decryption process failed!", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if (Flag == 1)//DES
+            {
+                string textBox1Name = textBox1.Text.Replace(".desencrypt", "");
+                if (DecryptData(textBox1.Text, textBox1Name, textBox3.Text) == true)
+                    MessageBox.Show(this, "Done!", "Decryption Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(this, "The decryption process failed!", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+
+        public bool EncryptData(String sourceFile, String destinationFile, String cryptoKey)
+        {
+            CryptoStream cryptoStream = null;
+
+                try
+                {
+                    //Откройте файл источника и назначения, используя объект потока файлов
+                    FileStream inFileStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+                    FileStream outFileStream = new FileStream(destinationFile, FileMode.Create, FileAccess.Write);
+                    outFileStream.SetLength(0);
+
+                    //Создаем переменные, чтобы помочь с чтением и записью .
+                    byte[] bin = new byte[100]; // Это промежуточное хранилище для шифрования.
+                    long rdlen = 0;  // Это общее количество написанных байтов.
+                    long totlen = inFileStream.Length; // Это общая длина входного файла. 
+                    int len;   //Это количество байтов, которые будут написаны одновременно .
+
+                    if (Flag == 0)//AES
+                    {
+                        //Создаем объект поставщика услуг AES и назначаем ключ и вектор к нему. 
+                        AesManaged AESProvider = new AesManaged();
+
+                        AESProvider.Key = ASCIIEncoding.ASCII.GetBytes(cryptoKey);
+                        // Устанавливаем вектор инициализации.
+                        AESProvider.IV = this.iv;
+                        ICryptoTransform AESEncrypt = AESProvider.CreateEncryptor(AESProvider.Key, AESProvider.IV);
+
+                        // Создаем класс CryptoStream и пишем зашифрованные 
+                        cryptoStream = new CryptoStream(outFileStream, AESEncrypt, CryptoStreamMode.Write);
+                    }
+                    else if (Flag == 1)//DES
+                    {
+                        //Создаем объект поставщика услуг DES и назначаем ключ и вектор к нему.
+                        DESCryptoServiceProvider DESProvider = new DESCryptoServiceProvider();
+
+                        DESProvider.Key = ASCIIEncoding.ASCII.GetBytes(cryptoKey);
+                        // Устанавливаем вектор инициализации.
+                        DESProvider.IV = this.iv3;
+                        ICryptoTransform DESEncrypt = DESProvider.CreateEncryptor(DESProvider.Key, DESProvider.IV);
+
+                        // Создаем класс CryptoStream и пишем зашифрованные
+                         cryptoStream = new CryptoStream(outFileStream, DESEncrypt, CryptoStreamMode.Write);
+                    }   
+
+                    // Читаем из входного файла,затем шифруем и записываем в выходной файл.
+                    while (rdlen < totlen)
+                    {
+                        len = inFileStream.Read(bin, 0, 100);
+                        cryptoStream.Write(bin, 0, len);
+                        rdlen = rdlen + len;
+                    }
+
+
+                    // Закрыть обработчики потока 
+                    cryptoStream.Close();
+                    inFileStream.Close();
+                    outFileStream.Close();
+                    return true;
+                }
+
+                catch (Exception e)
+                {
+                    MessageBox.Show(this, e.ToString(), "Encryption Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return false;
+                }
+                      
+        }
+
+
+        // The DecryptData method will decrypt the given file using the AES algorithm
+        public bool DecryptData(String sourceFile, String destinationFile, String cryptoKey)
+        {
+            CryptoStream cryptoStream = null;
+                try
+                {
+                    // Откройте файл источника и назначения, используя объект потока файлов
+                    FileStream inFileStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+                    FileStream outFileStream = new FileStream(destinationFile, FileMode.Create, FileAccess.Write);
+                    outFileStream.SetLength(0);
+
+                    //Создаем переменные, чтобы помочь с чтением и записью .
+                    byte[] bin = new byte[100]; // Это промежуточное хранилище для расшифрования.
+                    long rdlen = 0;  // Это общее количество написанных байтов.
+                    long totlen = inFileStream.Length; // Это общая длина входного файла. 
+                    int len;   //Это количество байтов, которые будут написаны одновременно .
+
+                if (Flag == 0)
+                {
+                    //Создаем объект поставщика услуг AES и назначаем ключ и вектор к нему.
+                    AesManaged AESProvider = new AesManaged();
+                    AESProvider.Key = ASCIIEncoding.ASCII.GetBytes(cryptoKey);
+                    // Устанавливаем вектор инициализации.
+                    AESProvider.IV = this.iv;
+                    ICryptoTransform AESDecrypt = AESProvider.CreateDecryptor(AESProvider.Key, AESProvider.IV);
+
+                     cryptoStream = new CryptoStream(outFileStream, AESDecrypt, CryptoStreamMode.Write);
+                }
+                else if (Flag == 1) 
+                {
+                    //Создаем объект поставщика услуг DES и назначаем ключ и вектор к нему.
+                    DESCryptoServiceProvider DESProvider = new DESCryptoServiceProvider();
+                    DESProvider.Key = ASCIIEncoding.ASCII.GetBytes(cryptoKey);
+                    // Устанавливаем вектор инициализации.
+                    DESProvider.IV = this.iv3;
+                    ICryptoTransform DESDecrypt = DESProvider.CreateDecryptor(DESProvider.Key, DESProvider.IV);
+
+                     cryptoStream = new CryptoStream(outFileStream, DESDecrypt, CryptoStreamMode.Write);
+                }
+
+                    //CryptoStream cryptoStream = new CryptoStream(outFileStream, AESDecrypt, CryptoStreamMode.Write);
+
+                    // Читаем из входного файла,затем шифруем и записываем в выходной файл.
+                    while (rdlen < totlen)
+                    {
+                        len = inFileStream.Read(bin, 0, 100);
+                        cryptoStream.Write(bin, 0, len);
+                        rdlen = rdlen + len;
+                    }
+
+                    // Закрыть обработчики потока 
+                    cryptoStream.Close();
+                    inFileStream.Close();
+                    outFileStream.Close();
+                    return true;
+                }
+
+                catch (Exception e)
+                {
+                    MessageBox.Show(this, e.ToString(), "Decryption Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return false;
+                }
+
+            
+
+
+        }
+
+        private void dESToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Flag = 1;// DES
+            textBox3.Text = "TfG5H6ip";
+        }
+
+        private void aESToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Flag = 0;// AES
+            textBox3.Text = "awsPay165FcfyTf6";
+        }
+    }
+}
